f82dd8ed21220f5de733ff461b0abc3f41f23ca3
Nissharfathima <38177009+Nissharfathima@users.noreply.github.com>
2024-02-02 17:04:58 +0530

    Axios example
    Fetch Users and display in the List . Search for the user and display the User name

--- frontendquest/src/App.js
+++ frontendquest/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useState, useEffect } from "react";
 
-function App() {
+export default function App() {
+  const [users, setUsers] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const [filteredUsers, setFilteredUsers] = useState([]);
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
+      console.log(res.data);
+      setUsers(res.data);
+    });
+  },[]);
+
+  const handleSearchText = (e) => {
+    setSearchText(e.target.value);
+    const filtered = users.filter((user) => user.name.includes(searchText));
+    console.log(filtered);
+    setFilteredUsers(filtered);
+  };
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>List of Users</h1>
+      Search User<input type={"text"} value={searchText} onChange={handleSearchText} />
+      {searchText
+        ? filteredUsers.map((user) => <p>{user.name}</p>)
+        : users.map((user) => (
+            <ul>
+              <li>{user.name}</li>
+            </ul>
+          ))}
     </div>
   );
 }
-
-export default App;
